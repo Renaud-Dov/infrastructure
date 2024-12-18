0510e059e5c2e926d63d5c76e5b745b378a6ece7
Dov Devers <contact@bugbear.fr>
2024-12-18 16:32:52 +0100

    terraform: mx_server: Add MX and CNAME records for ProtonMail
    Introduced Cloudflare MX records for ProtonMail mail servers and CNAME records for domain key configurations. This ensures proper email routing and DKIM authentication for the domain.

---
+++ terraform/mx_server.tf
@@ -0,0 +1,37 @@
+resource "cloudflare_record" "mail_mx" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = "@"
+  type    = "MX"
+  content = "mail.protonmail.ch"
+  priority = 10
+
+}
+
+resource "cloudflare_record" "mailsec_mx" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = "@"
+  type    = "MX"
+  content = "mailsec.protonmail.ch"
+  priority = 20
+}
+
+resource "cloudflare_record" "protonmail_domainkey" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = "protonmail._domainkey"
+  type    = "CNAME"
+  content = "protonmail.domainkey.dfjualtqrvxy5yfxd7awdgyecrj3klzhpwi4ixjtufbs46hjds3fq.domains.proton.ch."
+}
+
+resource "cloudflare_record" "protonmail2_domainkey" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = "protonmail2._domainkey"
+  type    = "CNAME"
+  content = "protonmail2.domainkey.dfjualtqrvxy5yfxd7awdgyecrj3klzhpwi4ixjtufbs46hjds3fq.domains.proton.ch."
+}
+
+resource "cloudflare_record" "protonmail3_domainkey" {
+  zone_id = data.cloudflare_zone.this.id
+  name    = "protonmail3._domainkey"
+  type    = "CNAME"
+  content = "protonmail3.domainkey.dfjualtqrvxy5yfxd7awdgyecrj3klzhpwi4ixjtufbs46hjds3fq.domains.proton.ch."
+}
